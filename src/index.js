3588ca00b7937c87ea8e56ec34f6ecc96711566f
SofianykDasha <91177584+SofianykDasha@users.noreply.github.com>
2024-05-19 18:16:04 +0300

    Add files via upload
    Fix searching, fix 'burger' menu, add fonts, do small refactoring

--- src/index.js
+++ src/index.js
@@ -11,44 +11,57 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   form.addEventListener("submit", handleSearch);
 
-  function createMarkup(arr) {
-    var array_makes = []
-    var array_models = []
-    var array_fuel_types = []
-
-    const list = arr.map(item => {
-      array_makes.push(item.car);
-      array_models.push(item.type);
-      array_fuel_types.push(item.fuel);
-  
-      return `
-        <li class="car-card" data-id="${item.id}">
-          <img src="${item.img}" alt="${item.type}" class="car-image">
-          <h2 class="car-title">${item.car}</h2>
-          <h3 class="car-type">${item.type}</h3>
-          <h3 class="car-mileage">${item.mileage}км</h3>
-          <h3 class="car-capacity"> Об'єм двигуна:${item.capacity}</h3>
-          <h3 class="car-fuel">Тип палива:${item.fuel}</h3>
-          <span class="car-price">${item.price}$</span>
-        </li>
-      `;
-    }).join("");
-    makes = [...new Set(array_makes)];
-    models = [...new Set(array_models)];
-    fuel_types = [...new Set(array_fuel_types)];
-
-    populateSelect('makes-select', makes);
-    populateSelect('models-select', models);
-    populateSelect('fuel-types-select', fuel_types);
-  
-    return list;
-  }
-
   container.style.display = "flex";
   container.style.flexWrap = "wrap";
   container.style.gap = "25px";
   container.insertAdjacentHTML("beforeend", createMarkup(cars));
+
+  populateSelect('makes-select', makes);
+  populateSelect('models-select', models);
+  populateSelect('fuel-types-select', fuel_types);
+
+  const menuIcon = document.getElementById('menu-icon');
+  const menu = document.getElementById('menu');
+
+  menuIcon.addEventListener('click', () => {
+    if (menu.classList.contains('show')) {
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+    } else {
+      menu.classList.remove('hide');
+      menu.classList.add('show');
+    }
+  });
 })
+
+function createMarkup(arr) {
+  var array_makes = []
+  var array_models = []
+  var array_fuel_types = []
+
+  const list = arr.map(item => {
+    array_makes.push(item.car);
+    array_models.push(item.type);
+    array_fuel_types.push(item.fuel);
+
+    return `
+      <li class="car-card" data-id="${item.id}">
+        <img src="${item.img}" alt="${item.type}" class="car-image">
+        <h2 class="car-title">${item.car}</h2>
+        <h3 class="car-type">${item.type}</h3>
+        <h3 class="car-mileage">${item.mileage}км</h3>
+        <h3 class="car-capacity"> Об'єм двигуна:${item.capacity}</h3>
+        <h3 class="car-fuel">Тип палива:${item.fuel}</h3>
+        <span class="car-price">${item.price}$</span>
+      </li>
+    `;
+  }).join("");
+  makes = [...new Set(array_makes)];
+  models = [...new Set(array_models)];
+  fuel_types = [...new Set(array_fuel_types)];
+
+  return list;
+}
 
 function populateSelect(selectId, options) {
   const select = document.getElementById(selectId);
@@ -63,14 +76,38 @@ function handleSearch(event) {
   event.preventDefault();
 
   const elements = event.target.elements;
-  const query = elements.query.value;
-  const option = elements.options.value;
-
-  const result = cars       
-    .filter(item => item[option].toLowerCase().includes(query.toLowerCase()))
-    .sort((a, b) => a[option].localeCompare(b[option]))
+  const make = elements.make.value == '-Вибір-' ? '' : elements.make.value;
+  const model = elements.model.value == '-Вибір-' ? '' : elements.model.value;
+  const mileage = elements.mileage.value;
+  const engineVolume = elements.engine_volume.value;
+  const fuelType = elements.fuel.value == '-Вибір-' ? '' : elements.fuel.value ;
   
-  container.innerHTML = createMarkup(result);
+  const result = cars       
+                  .filter(car => {
+                    return (!make || car.car.toLowerCase().includes(make.toLowerCase())) &&
+                          (!model || car.type.toLowerCase().includes(model.toLowerCase())) &&
+                          (!mileage || car.mileage <= parseInt(mileage, 10)) &&
+                          (!engineVolume || car.capacity == parseFloat(engineVolume)) &&
+                          (!fuelType || car.fuel.toLowerCase().includes(fuelType.toLowerCase()));
+                  });
+  const container = document.querySelector(".js-list");
+
+  if (result.length === 0) {
+    container.innerHTML = `<div>Нічого не знайдено :(</div>`;
+  } else {
+    container.innerHTML = createMarkup(result);
+  }
+}
+
+function handleWelcome(event) {
+  event.preventDefault();
+
+  let name = 'Taras'
+
+  let user = document.getElementById('user');
+
+  user.textContent = name;
+  
 }
 
 async function createCar() {
